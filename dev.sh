940acd1aaea5d27cf28be3f054de17abb56887c7
Eli Mallon <eli@livepeer.org>
2019-07-05 10:46:26 -0700

    Allow separate users in the dev.sh script

--- dev.sh
+++ dev.sh
@@ -5,11 +5,13 @@
 # set -o nounset
 set -e
 
-gsutil cp /Library/WowzaStreamingEngine/lib/LivepeerWowza.jar gs://livepeerjs-public/eli/LivepeerWowza.jar
-kubectl delete pod -l app=wowza --wait
-until kubectl logs --tail=0 deployment/wowza; do
+DEV_USER="$1"
+
+gsutil cp /Library/WowzaStreamingEngine/lib/LivepeerWowza.jar gs://livepeerjs-public/$DEV_USER/LivepeerWowza.jar
+kubectl delete pod -l app=$DEV_USER --wait
+until kubectl logs --tail=0 deployment/$DEV_USER; do
   echo "Waiting for pod to deploy..."
   sleep 1
 done
-kubectl port-forward deployment/wowza 1935:1935 8086:8086 8087:8087 8088:8088 8089:8089 &
-kubectl logs -f deployment/wowza
+kubectl port-forward deployment/$DEV_USER 1935:1935 8086:8086 8087:8087 8088:8088 8089:8089 &
+kubectl logs -f deployment/$DEV_USER
