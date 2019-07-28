c89bd9cf0724a5e534a3a20e8d5201b1e651fbc9
Eli Mallon <eli@livepeer.org>
2019-07-27 20:24:43 -0700

    Add uploaded JSON bits

--- hack/upload-build.sh
+++ hack/upload-build.sh
@@ -5,54 +5,42 @@
 set -e
 set -o nounset
 
-if [[ $(uname) == *"MSYS2_NT"* ]]; then
-  ARCH="windows"
-  EXT=".exe"
-else
-  ARCH=$(uname | tr '[:upper:]' '[:lower:]')
-  EXT=""
-fi
+# Required variables:
+# $GCLOUD_KEY
+# $GCLOUD_SECRET
+# $DISCORD_URL
+# $BRANCH
 
-BASE="livepeer-$ARCH-amd64"
-BRANCH="${TRAVIS_BRANCH:-${CIRCLE_BRANCH:-unknown}}"
-VERSION="$(cat VERSION)-$(git describe --always --long --abbrev=8 --dirty)"
+version="$(git describe --tags --always --dirty)"
+bucket="build.livepeer.live"
+set -x
 
-mkdir $BASE
-cp ./livepeer${EXT} $BASE
-cp ./livepeer_cli${EXT} $BASE
+# Adapted from https://stackoverflow.com/a/44751929/990590
+function doUpload() {
+  FILE="$1"
+  UPLOAD_PATH="$2"
+  CONTENT_TYPE="$3"
+  resource="/${bucket}/LivepeerWowza/${UPLOAD_PATH}"
+  dateValue=`date -R`
+  stringToSign="PUT\n\n${CONTENT_TYPE}\n${dateValue}\n${resource}"
+  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${GCLOUD_SECRET} -binary | base64`
+  curl --fail -X PUT -T "${FILE}" \
+    -H "Host: storage.googleapis.com" \
+    -H "Date: ${dateValue}" \
+    -H "Content-Type: ${CONTENT_TYPE}" \
+    -H "Authorization: AWS ${GCLOUD_KEY}:${signature}" \
+    https://storage.googleapis.com${resource}
+}
 
-# do a basic upload so we know if stuff's working prior to doing everything else
-if [[ $ARCH == "windows" ]]; then
-  FILE=$BASE.zip
-  # This list was produced by `ldd livepeer.exe`
-  LIBS="libffi-6.dll libgcc_s_seh-1.dll libgmp-10.dll libgnutls-30.dll libhogweed-4.dll libiconv-2.dll libidn2-0.dll libintl-8.dll libnettle-6.dll libp11-kit-0.dll libtasn1-6.dll libunistring-2.dll libwinpthread-1.dll zlib1.dll"
-  for LIB in $LIBS; do
-    cp -r /mingw64/bin/$LIB ./$BASE
-  done
-  zip -r ./$FILE ./$BASE
-else
-  FILE=$BASE.tar.gz
-  tar -czvf ./$FILE ./$BASE
-fi
+echo "
+{
+  \"version\": \"${version}\",
+  \"url\": \"https://$bucket/LivepeerWowza/$BRANCH/$version/LivepeerWowza.jar\"
+}
+" > uploadVersion.json 
 
-if [[ "${GCLOUD_KEY:-}" == "" ]]; then
-  echo "GCLOUD_KEY not found, not uploading to Google Cloud."
-  exit 0
-fi
+doUpload LivepeerWowza.jar "$BRANCH/$version/LivepeerWowza.jar" "application/x-compressed-tar"
+doUpload uploadVersion.json "$BRANCH.json" "application/json"
 
-# https://stackoverflow.com/a/44751929/990590
-bucket=build.livepeer.live
-resource="/${bucket}/${VERSION}/${FILE}"
-contentType="application/x-compressed-tar"
-dateValue=`date -R`
-stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${GCLOUD_SECRET} -binary | base64`
-curl -X PUT -T "${FILE}" \
-  -H "Host: storage.googleapis.com" \
-  -H "Date: ${dateValue}" \
-  -H "Content-Type: ${contentType}" \
-  -H "Authorization: AWS ${GCLOUD_KEY}:${signature}" \
-  https://storage.googleapis.com${resource}
-
-curl --fail -s -H "Content-Type: application/json" -X POST -d "{\"content\": \"Build succeeded ✅\nBranch: $BRANCH\nPlatform: $ARCH-amd64\nLast commit: $(git log -1 --pretty=format:'%s by %an')\nhttps://build.livepeer.live/$VERSION/${FILE}\"}" $DISCORD_URL 2>/dev/null
+curl --fail -s -H "Content-Type: application/json" -X POST -d "{\"content\": \"Build succeeded ✅\nBranch: $BRANCH\nLast commit: $(git log -1 --pretty=format:'%s by %an')\nhttps://build.livepeer.live/LivepeerWowza/$BRANCH/$version/LivepeerWowza.jar\"}" $DISCORD_URL 2>/dev/null
 echo "done"
