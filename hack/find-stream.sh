846bfc790b0918e5d3980c2bab863c422640ae5d
Eli Mallon <eli@livepeer.org>
2019-07-30 10:15:35 -0700

    improve find-stream.sh

--- hack/find-stream.sh
+++ hack/find-stream.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 broadcasters=$(curl https://livepeer.live/api/broadcaster | jq -r '.[].address')
 for broadcaster in $broadcasters; do
   url="$broadcaster/stream/$1.m3u8"
-  curl $url
+  if curl --fail --silent $url > /dev/null; then
+    echo "$broadcaster/stream/$1.m3u8"
+    echo "$broadcaster/stream/$1/P360p30fps16x9.m3u8"
+    echo "$broadcaster/stream/$1/P240p30fps16x9.m3u8"
+  fi
 done
